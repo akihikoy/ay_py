281d356ea8ec8ed09b6e3cdc74031fa2e9bf3c3f
Akihiko Yamaguchi <info@akihikoy.net>
2022-05-07 03:11:15 +0900

    Added a virtual joystick widget into the simple Qt panel;

--- src/ay_py/tool/py_panel.py
+++ src/ay_py/tool/py_panel.py
@@ -254,6 +254,163 @@ class TPrimitivePainter(QtGui.QWidget):
     painter.setBrush(QtCore.Qt.NoBrush)
     painter.drawRect(QtCore.QRect(0, 0, self.width() - 1, self.height() - 1))
 
+class TVirtualJoyStick(QtGui.QWidget):
+  onstickmoved= QtCore.pyqtSignal(list)
+  def __init__(self, parent=None):
+    super(TVirtualJoyStick, self).__init__(parent)
+    #self.setMinimumSize(100, 100)
+    self.setBackgroundRole(QtGui.QPalette.Base)
+
+    self.kind= 'circle'  #circle, ellipse, hbox, vbox
+    self.bg_color= [200]*3  #Color of movable range.
+    self.bg_height= 0.3  #Display height of movable range (effective with kind==hbox,vbox).
+    self.resetStickPos()
+    self.stick_grabbed= False
+    self.stick_size= 0.6  #Stick size per width/height of movable range.
+    self.stick_color= [128, 128, 255]
+
+  #kind: 2d-joy: circle, ellipse, 1d-joy: hbox, vbox
+  def setKind(self, kind):
+    self.kind= kind
+
+  #Set color of movable range.
+  def setBGColor(self, bg_color):
+    self.bg_color= bg_color
+
+  #Set display height of movable range (effective with kind==hbox,vbox).
+  def setBGHeight(self, bg_height):
+    self.bg_height= bg_height
+
+  #Set stick size per width/height of movable range.
+  def setStickSize(self, stick_size):
+    self.stick_size= stick_size
+
+  def setStickColor(self, stick_color):
+    self.stick_color= stick_color
+
+  def resetStickPos(self):
+    #Normalized stick position:
+    self.stick_pos= QtCore.QLineF(QtCore.QPointF(0,0),QtCore.QPointF(0,0))
+
+  def center(self):
+    return QtCore.QPointF(self.width()/2, self.height()/2)
+
+  def minimumSizeHint(self):
+    #return QtCore.QSize(100, self.heightForWidth(100))
+    return QtCore.QSize(20, 20)
+
+  #def sizeHint(self):
+    #return QtCore.QSize(400, self.heightForWidth(400))
+
+  #def heightForWidth(self, width):
+    #return width*1.2
+
+  #Return the current joystick position.
+  def position(self):
+    #return self.stick_pos.p2(), self.stick_pos.length(), self.stick_pos.angle()
+    if self.kind in ('ellipse','circle'):
+      return [self.stick_pos.p2().x(), self.stick_pos.p2().y()]
+    elif self.kind=='hbox':
+      return [self.stick_pos.p2().x()]
+    elif self.kind=='vbox':
+      return [self.stick_pos.p2().y()]
+
+  def getGradient(self, color, bounds, reverse=False):
+    col0= QtGui.QColor(min(255,1.5*color[0]), min(255,1.5*color[1]), min(255,1.5*color[2]))
+    col1= QtGui.QColor(*color)
+    col2= QtGui.QColor(0.6*color[0], 0.6*color[1], 0.6*color[2])
+    col3= QtGui.QColor(0.2*color[0], 0.2*color[1], 0.2*color[2])
+    positions= [0.0,0.2,0.8,1.0]
+    #colors= [QtCore.Qt.white,col1,col2,QtCore.Qt.black]
+    colors= [col0,col1,col2,col3]
+    linear_gradient= QtGui.QLinearGradient(bounds.topLeft(), bounds.bottomRight())
+    for pos,col in zip(positions, colors if not reverse else reversed(colors)):
+      linear_gradient.setColorAt(pos, col)
+    return linear_gradient
+
+  def getMovableRange(self):
+    if self.kind=='ellipse':
+      return self.width(), self.height()
+    elif self.kind=='circle':
+      l= min(self.width(), self.height())
+      return l,l
+    elif self.kind=='hbox':
+      return self.width(), self.width()*self.bg_height
+    elif self.kind=='vbox':
+      return self.height()*self.bg_height, self.height()
+
+  def getStickSize(self):
+    mw,mh= self.getMovableRange()
+    if self.kind in ('ellipse','circle'):
+      return mw*self.stick_size, mw*self.stick_size
+    elif self.kind=='hbox':
+      return mw*self.stick_size, mh
+    elif self.kind=='vbox':
+      return mw, mh*self.stick_size
+
+  def getStickRange(self):
+    mw,mh= self.getMovableRange()
+    sw,sh= self.getStickSize()
+    if self.kind in ('ellipse','circle'):
+      return mw/2.0-sw/2.0, mh/2.0-sh/2.0
+    elif self.kind=='hbox':
+      return mw/2.0-sw/2.0, 0
+    elif self.kind=='vbox':
+      return 0, mh/2.0-sh/2.0
+
+  def paintEvent(self, event):
+    painter= QtGui.QPainter(self)
+    painter.setPen(QtCore.Qt.SolidLine)
+    painter.setRenderHint(QtGui.QPainter.Antialiasing)
+
+    #Drawing movable range.
+    mw,mh= self.getMovableRange()
+    bounds= QtCore.QRectF(-mw/2+1, -mh/2+1, mw-2, mh-2).translated(self.center())
+    painter.setBrush(self.getGradient(self.bg_color, bounds, reverse=True))
+    if self.kind in ('ellipse','circle'):
+      painter.drawEllipse(bounds)
+    elif self.kind in ('hbox','vbox'):
+      painter.drawRect(bounds)
+
+    #Drawing stick.
+    sx,sy= self.getStickRange()
+    stick_displacement= self.center()+QtCore.QPointF(self.stick_pos.p2().x()*sx,-self.stick_pos.p2().y()*sy)
+    sw,sh= self.getStickSize()
+    bounds= QtCore.QRectF(-sw/2.0+1, -sh/2.0+1, sw-2, sh-2).translated(stick_displacement)
+    painter.setBrush(self.getGradient(self.stick_color, bounds))
+    #painter.setBrush(QtCore.Qt.black)
+    if self.kind in ('ellipse','circle'):
+      painter.drawEllipse(bounds)
+    elif self.kind in ('hbox','vbox'):
+      painter.drawRect(bounds)
+
+  def mousePressEvent(self, event):
+    dp= QtCore.QLineF(self.center(), event.pos())
+    sw,sh= self.getStickSize()
+    if self.kind in ('ellipse','circle'):
+      self.stick_grabbed= dp.length()<sw
+    elif self.kind in ('hbox','vbox'):
+      self.stick_grabbed= abs(dp.dx())<sw/2.0 and abs(dp.dy())<sh/2.0
+    if self.stick_grabbed:  self.pos_grabbed= event.pos()
+    self.onstickmoved.emit(self.position())
+    return super(TVirtualJoyStick, self).mousePressEvent(event)
+
+  def mouseReleaseEvent(self, event):
+    self.stick_grabbed= False
+    self.resetStickPos()
+    self.update()
+    self.onstickmoved.emit(self.position())
+
+  def mouseMoveEvent(self, event):
+    if self.stick_grabbed:
+      dp= QtCore.QPointF(event.pos()-self.pos_grabbed)
+      sx,sy= self.getStickRange()
+      self.stick_pos= QtCore.QLineF(QtCore.QPointF(0,0), QtCore.QPointF(dp.x()/sx if sx>0 else 0,-dp.y()/sy if sy>0 else 0))
+      if self.stick_pos.length()>1.0:  self.stick_pos.setLength(1.0)
+      self.update()
+      self.onstickmoved.emit(self.position())
+    #print self.position()
+
 if 'rviz' in sys.modules:
   class TRVizUtil(rviz.VisualizationFrame):
     def __init__(self):
@@ -321,6 +478,7 @@ class TSimplePanel(QtGui.QWidget):
       'label': self.AddLabel,
       'textedit': self.AddTextEdit,
       'primitive_painer': self.AddPrimitivePainter,
+      'virtual_joystick': self.AddVirtualJoyStick,
       'rviz': self.AddRViz,
       }
     self.resize(*size)  #window size
@@ -562,6 +720,25 @@ class TSimplePanel(QtGui.QWidget):
     self.ApplyCommonWidgetConfig(primitive, param)
     return primitive
 
+  def AddVirtualJoyStick(self, w_param):
+    param= MergeDict2(copy.deepcopy(self.param_common), {
+      'kind':'circle',  #kind: 2d-joy: circle, ellipse, 1d-joy: hbox, vbox
+      'bg_color':[200,200,200],
+      'bg_height':0.3,  #Display height of movable range (effective with kind==hbox,vbox).
+      'stick_size':0.6,  #Stick size per width/height of movable range.
+      'stick_color':[128,128,255],
+      'onstickmoved':None,
+      }, w_param)
+    joy= TVirtualJoyStick(self)
+    joy.setKind(param['kind'])
+    joy.setBGColor(param['bg_color'])
+    joy.setBGHeight(param['bg_height'])
+    joy.setStickSize(param['stick_size'])
+    joy.setStickColor(param['stick_color'])
+    if param['onstickmoved']:  joy.onstickmoved.connect(lambda pos=None: param['onstickmoved'](self,joy))
+    self.ApplyCommonWidgetConfig(joy, param)
+    return joy
+
   def AddRViz(self, w_param):
     param= MergeDict2(copy.deepcopy(self.param_common), {
       'config':None,
@@ -748,6 +925,28 @@ if __name__=='__main__':
     #'rviz1': (
       #'rviz',{
         #'setup': True,  }),
+    'joy_xy1': (
+      'virtual_joystick',{
+        'kind':'circle',
+        'stick_color':[128,128,255],
+        'onstickmoved': lambda w,obj:Print('Pos:',obj.position()), }),
+    'joy_xy2': (
+      'virtual_joystick',{
+        'kind':'circle',
+        'stick_color':[255,128,128],
+        'onstickmoved': lambda w,obj:Print('Pos:',obj.position()), }),
+    'joy_h1': (
+      'virtual_joystick',{
+        'kind':'hbox',
+        'stick_color':[128,255,128],
+        'size_policy': ('expanding', 'minimum'),
+        'onstickmoved': lambda w,obj:Print('Pos:',obj.position()), }),
+    'joy_v1': (
+      'virtual_joystick',{
+        'kind':'vbox',
+        'stick_color':[128,255,128],
+        'size_policy': ('minimum', 'expanding'),
+        'onstickmoved': lambda w,obj:Print('Pos:',obj.position()), }),
     'spacer1': ('spacer', {}),
     }
   #Layout option: vbox, hbox, grid, tab
@@ -772,6 +971,11 @@ if __name__=='__main__':
             ('tab2', ('boxv',None, ('label_tab2', 'btn_totab10', 'btn_totab31', 'checkbox1') ) ),
             ('tab3', ('boxv',None, ('primitive_painer1','btn_totab11', 'btn_totab21', 'textedit1') ) ),
             #('tab4', ('boxv',None, ('rviz1',) ) ),
+            ('tab5', ('boxv',None,
+                        (
+                          ('boxh',None, ( ('boxh',None, ('joy_xy1','joy_xy2')),'joy_v1')),
+                          'joy_h1',
+                        ) ) ),
             ))
 
   InitPanelApp()
